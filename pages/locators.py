39d37b0da8325e0a7a06f1c7689789d1893272de
IV <i.v.boorlaka@gmail.com>
2022-09-24 19:05:16 +0300

    Task-9, refactor BasePage, add tests ProductPage

--- pages/locators.py
+++ pages/locators.py
@@ -1,13 +1,20 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators:
+class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
+class MainPageLocators:
+    pass
 
 
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    LOGIN_FORM_INVALID = (By.CSS_SELECTOR, "#login_form_inc")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTER_FORM_INVALID = (By.CSS_SELECTOR, "#register_form_inc")
 
 
 class ProductPageLocators:
@@ -17,3 +24,4 @@ class ProductPageLocators:
     PRICE_BOOK = (By.CSS_SELECTOR, ".col-sm-6  .price_color")
     BASKET_TOTAL = (By.CSS_SELECTOR, "div[class='alertinner '] p strong")
     NAME_BOOK_IN_BASKET = (By.CSS_SELECTOR, "#messages>div:nth-child(1)>div>strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success")
--- pages/product_page.py
+++ pages/product_page.py
@@ -48,3 +48,11 @@ class ProductPage(BasePage):
     def should_be_price_product_in_basket(self):
         assert self.get_price_basket_total() == self.get_price_product(), \
             "Price cart does not is same as price of product"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_disappeared_be_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is disappeared, but should not be"
